e74d063d55cd4c7b39ffd6c435437d3345f8a580
Colin Bounouar <colin.bounouar@external.engie.com>
2016-12-06 11:17:06 +0100

    Document pyxelrest methods

--- src/main/python/pyxelrest/pyxelrest.py
+++ src/main/python/pyxelrest/pyxelrest.py
@@ -4,7 +4,7 @@ from configparser import ConfigParser
 import requests
 from jinja2 import Environment, FileSystemLoader
 
-# Some parameter names might be VBA keywords
+# Some parameter names might be VBA keywords, this dictionary is used to use valid VBA parameter names.
 vba_restricted_keywords = {
     'currency': 'currency_visual_basic'
 }
@@ -12,6 +12,11 @@ vba_restricted_keywords = {
 
 class SwaggerService:
     def __init__(self, udf_prefix, config):
+        """
+        Load service information from configuration and swagger JSON.
+        :param udf_prefix: Prefix to use in front of services UDFs to avoid duplicate between services.
+        :param config: ConfigParser instance from where service details are retrieved.
+        """
         section = config[udf_prefix]
         self.udf_prefix = udf_prefix
 
@@ -28,18 +33,38 @@ class SwaggerService:
         self.swagger = self._retrieve_swagger(swagger_suffix)
 
     def _retrieve_swagger(self, swagger_suffix):
+        """
+        Retrieve swagger JSON from service.
+        :param swagger_suffix: URI of the swagger JSON on the service.
+        :return: Dictionary representation of the retrieved swagger JSON.
+        """
         response = requests.get(self.uri + swagger_suffix)
         response.raise_for_status()
         swagger = response.json()
-        for methods in swagger['paths'].values():
+        self._update_vba_restricted_keywords(swagger)
+        return swagger
+
+    @classmethod
+    def _update_vba_restricted_keywords(cls, swagger_json):
+        """
+        Update name of parameters in the swagger JSON to avoid using VBA restricted keywords as parameter names.
+        :param swagger_json: Dictionary representing the swagger JSON.
+        :return: None
+        """
+        for methods in swagger_json['paths'].values():
             for method in methods.values():
                 for parameter in method['parameters']:
                     if parameter['name'] in vba_restricted_keywords:
                         parameter['name'] = vba_restricted_keywords[parameter['name']]
-        return swagger
 
 
 def load_services(config_file_path):
+    """
+    Retrieve swagger JSON for each service defined in configuration file.
+    :param config_file_path: Absolute file path (related to this file) to configuration file.
+    :return: List of SwaggerService objects, size is the same one as the number of sections within configuration file
+    (DEFAULT excluded).
+    """
     config_parser = ConfigParser()
     file_path = os.path.abspath(config_file_path)
     if not config_parser.read(file_path):
@@ -48,6 +73,10 @@ def load_services(config_file_path):
 
 
 def user_defined_functions():
+    """
+    Create xlwings User Defined Functions according to user_defined_functions template.
+    :return: A string containing python code with all xlwings UDFs.
+    """
     renderer = Environment(loader=FileSystemLoader(os.path.dirname(__file__)), trim_blocks=True)
     return renderer.get_template('user_defined_functions.tpl').render(
         current_utc_time=datetime.datetime.utcnow().isoformat(),
@@ -57,6 +86,10 @@ def user_defined_functions():
 
 
 def generate_user_defined_functions():
+    """
+    Create user_defined_functions.py python file containing generated xlwings User Defined Functions.
+    :return: None
+    """
     with open(os.path.join(os.path.dirname(__file__), 'user_defined_functions.py'), 'w') as generated_file:
         generated_file.write(user_defined_functions())
 
@@ -65,6 +98,6 @@ generate_user_defined_functions()
 
 from src.main.python.pyxelrest.user_defined_functions import *
 
-# Uncomment to debug Microsoft Excel UDF calls
+# Uncomment to debug Microsoft Excel UDF calls.
 # if __name__ == '__main__':
 #     xw.serve()
