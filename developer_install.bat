9812862406a012dd053653aa97a0ecd12f0fcdc1
Colin Bounouar <colin.bounouar@external.engie.com>
2017-01-16 18:05:42 +0100

    Add the ability to force update for developer

--- developer_install.bat
+++ developer_install.bat
@@ -2,13 +2,21 @@
 CLS
 ECHO 1.Install
 ECHO 2.Update
+ECHO 3.Update (force reinstall)
 ECHO.
 
-CHOICE /C 12 /M "Enter your choice:"
+CHOICE /C 123 /M "Enter your choice:"
 
 :: Note - list ERRORLEVELS in decreasing order
+IF ERRORLEVEL 3 GOTO ForceUpdate
 IF ERRORLEVEL 2 GOTO Update
 IF ERRORLEVEL 1 GOTO Install
+
+:ForceUpdate
+pip install pypiwin32 --trusted-host rms.gdfsuez.net --index http://rms.gdfsuez.net:8310/artifactory/api/pypi/python/simple --upgrade
+pip install -e . --trusted-host rms.gdfsuez.net --index http://rms.gdfsuez.net:8310/artifactory/api/pypi/python/simple --upgrade --force-reinstall
+python pyxelrest_post_install.py --addindirectory addin/AutoLoadPyxelRestAddIn/bin/Release --vbaddindirectory addin
+GOTO End
 
 :Update
 pip install pypiwin32 --trusted-host rms.gdfsuez.net --index http://rms.gdfsuez.net:8310/artifactory/api/pypi/python/simple --upgrade
