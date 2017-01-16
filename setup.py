5e2cea993d7bad7a832b3f91408cf5c2934572f2
Colin Bounouar <colin.bounouar@external.engie.com>
2017-01-16 10:25:28 +0100

    Rely on xlwings 0.10.2

--- setup.py
+++ setup.py
@@ -93,7 +93,7 @@ setup(name='pyxelrest',
       install_requires=[
           'jinja2>=2.8',
           'requests>=2.12.2',
-          'xlwings==0.10.1'
+          'xlwings==0.10.2'
       ],
       scripts=[
           'pyxelrest_post_install.py'
