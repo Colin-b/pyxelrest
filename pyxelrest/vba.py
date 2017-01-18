13ca53461331fb0648c2ad1a596af80cd3e5b046
Colin Bounouar <colin.bounouar@external.engie.com>
2017-01-18 19:06:40 +0100

    Avoid failure due to "end" VBA keyword used as a parameter

--- pyxelrest/vba.py
+++ pyxelrest/vba.py
@@ -1,4 +1,5 @@
 # Some parameter names might be VBA keywords, this dictionary is used to use valid VBA parameter names.
 vba_restricted_keywords = {
-    'currency': 'currency_visual_basic'
+    'currency': 'currency_visual_basic',
+    'end': 'end_visual_basic'
 }
