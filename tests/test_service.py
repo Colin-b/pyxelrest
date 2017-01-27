04c4bfaef82afb07409d85e3da50ed29c6b62881
Colin Bounouar <colin.bounouar@external.engie.com>
2017-01-27 15:44:49 +0100

    Allow to start test service manually as well

--- tests/test_service.py
+++ tests/test_service.py
@@ -12156,13 +12156,16 @@ def swagger_version_not_provided():
 def swagger_version_not_supported():
     return jsonify(swagger='1.0',
                    paths={
-        '/test/should/not/be/available': {
-            'get': {
-                'operationId': 'get_test_should_not_be_available'
-            }
-        },
-    })
+                       '/test/should/not/be/available': {
+                           'get': {
+                               'operationId': 'get_test_should_not_be_available'
+                           }
+                       }
+                   })
 
 
 def start_server(port):
     app.run(port=port)
+
+if __name__ == '__main__':
+    start_server(8943)
