7d04f61dfe43d0f5500b88671e7927c15cd6f145
Sébastien de Menten <sdementen@gmail.com>
2017-05-18 16:39:34 +0200

    add wait method to wait for end of page loading add 20sec limit to login process in IE

--- pyxelrest/ie.py
+++ pyxelrest/ie.py
@@ -12,7 +12,8 @@ class IE:
     Using UWND instead of LocationURL as more stable."""
 
     def __init__(self):
-        object.__setattr__(self, "_hwnd", Dispatch("InternetExplorer.Application").HWND)
+        ie = Dispatch("InternetExplorer.Application")
+        object.__setattr__(self, "_hwnd", ie.HWND)
 
     def __getattr__(self, item):
         try:
@@ -27,6 +28,11 @@ class IE:
         except pywintypes.com_error:
             del self.ie
             setattr(self, item, value)
+
+    def wait(self):
+        """Wait for end of page loading"""
+        while (self.ReadyState != 4):
+            time.sleep(0.25)
 
     @property
     def ie(self):
@@ -60,64 +66,41 @@ class IE:
 
         return ie
 
-
-#
-# def call(url,
-#          msg_auth_asked=None,
-#          msg_auth_done=None,
-#          url_auth_done=None):
-#     ie = IE()
-#
-#     ie.navigate(url)
-#
-#     while (ie.ReadyState != 4):
-#         time.sleep(0.1)
-#     logging.info("here")
-#     # click on login button
-#     # for btn in ie.Document.Body.getElementsByClassName("btn-link"):
-#     #     btn.click()
-#     print("first check", (msg_auth_asked and msg_auth_asked in ie.document.body.innerText))
-#     if (msg_auth_asked and msg_auth_asked in ie.document.body.innerText):
-#         popup_for_auth = True
-#     else:
-#         if url_auth_done:
-#             while (ie.LocationURL != url_auth_done):
-#                 print("waiting to land on", url_auth_done, ie.LocationURL)
-#                 time.sleep(2)
-#             popup_for_auth = False
-#         else:
-#             popup_for_auth = not (msg_auth_done and msg_auth_done in ie.document.body.innerText)
-#
-#     print("need to popup", popup_for_auth)
-#     if popup_for_auth:
-#         print("pop up for auth")
-#         ie.Visible = 1
-#     else:
-#         print("already auth")
-#         ie.Quit()
-#
-#     ie = None
-#
-#     return True
-
 def call_till_redirect(url_origin,
                        url_redirect):
     """Open IE on url_origin and close IE once url_redirect is loaded."""
-    logging.debug("Opening IE")
-    ie = IE()
+    logging.info("Opening IE")
+    try:
+        ie = IE()
+    except pywintypes.com_error:
+        return False
 
-    logging.debug("Navigating to {}".format(url_origin))
+
+    logging.info("Navigating to {}".format(url_origin))
     ie.navigate(url_origin)
 
-    while (ie.LocationURL != url_redirect):
-        logging.debug("Waiting to land on {}".format(url_redirect))
-        time.sleep(0.25)
+    ie.wait()
 
-    while (ie.ReadyState != 4):
-        logging.debug("Waiting redirect page fully loaded")
-        time.sleep(0.1)
+    count = 0.
+    sleep_time = 1
+    max_time = 20.
+    # exit loop after max_time seconds
+    while (ie.LocationURL != url_redirect) and (count <= max_time):
+        logging.info("Waiting to land on {}, still on {} : {} / {}".format(
+            url_redirect, ie.LocationURL,
+            url_redirect != ie.LocationURL,
+            count
+        ))
+        time.sleep(sleep_time)
+        count += sleep_time
 
-    logging.debug("Closing IE")
+    if count > max_time:
+        ie.Quit()
+        return False
+
+    ie.wait()
+
+    logging.info("Closing IE")
     ie.Quit()
 
     ie = None
