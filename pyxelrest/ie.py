806e33a942c5a95561c33f4500408328a2ad158f
Colin Bounouar <colin.bounouar@external.engie.com>
2017-05-30 16:44:24 +0200

    Use timeout provided by client for authentication on IE

--- pyxelrest/ie.py
+++ pyxelrest/ie.py
@@ -78,52 +78,77 @@ class IE:
         return ie
 
 
-def call_till_redirect(url_origin,
-                       url_redirect,
-                       max_time=30.
-                       ):
-    """Open IE on url_origin and close IE once url_redirect is loaded if within max_time seconds"""
-    logging.debug("Opening IE")
+def open_url(url):
+    """
+    Open IE on url.
+
+    :param url: Initial URL the browser will be opened onto.
+    :return: IE instance if opened.
+    """
+    logging.debug("Opening Microsoft Internet Explorer...")
     try:
         ie = IE()
     except pywintypes.com_error:
-        return False
+        return
 
-    logging.debug("Navigating to {}".format(url_origin))
-    ie.navigate(url_origin)
+    logging.debug("Navigating to {}.".format(url))
+    ie.navigate(url)
+    return ie
 
-    ie.wait()
 
-    count = 0.
-    sleep_time = 1.
-    # exit loop after max_time seconds
-    while (ie.location_url != url_redirect) and (count <= max_time):
-        logging.debug("Waiting to land on {}, still on {} [{}] : {} / {}".format(
-            url_redirect, ie.location_url,
-            ie.Document.Title,
-            url_redirect != ie.location_url,
-            count
-        ))
-        time.sleep(sleep_time)
-        count += sleep_time
-
-    if count > max_time:
-        ie.Quit()
-        logging.debug("Log failed within time constraints!")
-        return False
-
-    logging.debug("Log successfully finished!")
-    ie.wait()
-
-    logging.debug("Closing IE")
+def close(ie):
+    logging.debug("Closing Microsoft Internet Explorer...")
     ie.Quit()
-
+    # Force release of underlying Microsoft Internet Explorer instance
     ie = None
 
+
+def wait_until_url_is(ie, url, timeout, time_between_checks=1):
+    ie.wait()
+
+    current_awaited_time = 0
+    # exit loop after max_time seconds
+    while (ie.location_url != url) and (current_awaited_time <= timeout):
+        logging.debug("Waiting for URL to be {0}... Current one is still {1} ({2}), Awaited {3} seconds on {4}.".format(
+            url,
+            ie.Document.Title,
+            ie.location_url,
+            current_awaited_time,
+            timeout
+        ))
+        time.sleep(time_between_checks)
+        current_awaited_time += time_between_checks
+
+    if current_awaited_time > timeout:
+        logging.debug("URL was still not {0} after {1} seconds.".format(url, current_awaited_time))
+        return False
+
+    logging.debug("URL is now {0}.".format(url))
+    ie.wait()
     return True
+
+
+def call_till_redirect(url_origin, url_redirect, max_time):
+    """
+    Open IE on url_origin and close IE once url_redirect is loaded.
+
+    :param url_origin: Initial URL the browser will be opened onto.
+    :param url_redirect: The redirection URL
+    :param max_time: Maximum time to wait (in seconds) for redirection to be performed.
+    :return: If redirection succeeded within imparted time.
+    """
+    ie = open_url(url_origin)
+
+    if ie:
+        was_redirected = wait_until_url_is(ie, url_redirect, max_time)
+        if was_redirected:
+            ie.wait()  # Wait for page to be fully loaded
+        close(ie)
+        return was_redirected
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     call_till_redirect('https://gemgo.gemservices.io/',
-                       'https://gemgo.gemservices.io/')
+                       'https://gemgo.gemservices.io/',
+                       30)
