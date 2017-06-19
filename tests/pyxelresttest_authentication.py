530977dccda786ad1837d75675a56f2177f0940a
Colin Bounouar <colin.bounouar@external.engie.com>
2017-06-19 17:46:14 +0200

    Assert that OAuth2 authentication success test case is only checking authentication success now Add a new test case checking that if a custom response type is requested by service, it is properly handled by PyxelRest Add an additional test case to assert that a token is not requested once again when still valid (not expired) Increase OAuth2 token expiry test case determinism by waiting for 6 seconds to ensure that token that was valid for 5 seconds is properly expired Fasten OAuth2 authentication timeout test case by setting the timeout to 5s for the test case (instead of 20s)

--- tests/pyxelresttest_authentication.py
+++ tests/pyxelresttest_authentication.py
@@ -42,6 +42,18 @@ class PyxelRestAuthenticationTest(unittest.TestCase):
 
     def test_oauth2_authentication_success(self):
         import pyxelrestgenerator
+        token = pyxelrestgenerator.authenticated_test_get_test_oauth2_authentication_success()
+        self.assertEqual(token[0], ['Bearer'])
+        self.assertIsNotNone(token[1])
+
+    def test_oauth2_authentication_success_with_custom_response_type(self):
+        import pyxelrestgenerator
+        token = pyxelrestgenerator.authenticated_test_get_test_oauth2_authentication_success_with_custom_response_type()
+        self.assertEqual(token[0], ['Bearer'])
+        self.assertIsNotNone(token[1])
+
+    def test_oauth2_authentication_token_reuse(self):
+        import pyxelrestgenerator
         first_token = pyxelrestgenerator.authenticated_test_get_test_oauth2_authentication_success()
         self.assertEqual(first_token[0], ['Bearer'])
         # As the token should not be expired, this call should use the same token
@@ -74,7 +86,7 @@ class PyxelRestAuthenticationTest(unittest.TestCase):
         # This token will expires in 5 seconds
         first_token = pyxelrestgenerator.authenticated_test_get_test_oauth2_authentication_success_quick_expiry()
         self.assertEqual(first_token[0], ['Bearer'])
-        time.sleep(5)
+        time.sleep(6)
         # Token should now be expired, a new one should be requested
         second_token = pyxelrestgenerator.authenticated_test_get_test_oauth2_authentication_success_quick_expiry()
         self.assertEqual(second_token[0], ['Bearer'])
