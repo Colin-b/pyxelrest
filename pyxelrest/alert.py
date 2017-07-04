a75564df69fefff8f7a95b378069002e264d928d
Colin Bounouar <colin.bounouar@external.engie.com>
2017-07-04 19:26:16 +0200

    Add test case COM Server exception handling

--- pyxelrest/alert.py
+++ pyxelrest/alert.py
@@ -1,23 +1,30 @@
 import ctypes
 import sys
 
-MB_OK = 0x0
-MB_OKCXL = 0x01
-MB_YESNOCXL = 0x03
-MB_YESNO = 0x04
-MB_HELP = 0x4000
+# If set to True, calling message_box will have no effect. Can be useful for an application running on non-graphical env
+HIDE_MESSAGE_BOX = False
+
+BUTTON_OK = 0x0
+BUTTON_OK_CANCEL = 0x01
+BUTTON_YES_NO_CANCEL = 0x03
+BUTTON_YES_NO = 0x04
+BUTTON_HELP = 0x4000
+
 ICON_EXCLAIM = 0x30
 ICON_INFO = 0x40
 ICON_STOP = 0x10
-MB_SYSTEMMODAL = 0x1000
-MB_SETFOREGROUND = 0x10000
-MB_TOPMOST = 0x40000
+
+BEHAVIOR_SYSTEM_MODAL = 0x1000
+POSITION_FOREGROUND = 0x10000
+POSITION_TOP_MOST = 0x40000
 
 
 def message_box(title, message):
+    if HIDE_MESSAGE_BOX:
+        return
     if sys.version_info.major > 2:
-        ctypes.windll.user32.MessageBoxW(0, message, title, MB_OK | ICON_EXCLAIM | MB_SYSTEMMODAL)
+        ctypes.windll.user32.MessageBoxW(0, message, title, BUTTON_OK | ICON_EXCLAIM | BEHAVIOR_SYSTEM_MODAL)
     else:
-        ctypes.windll.user32.MessageBoxA(0, message, title, MB_OK | ICON_EXCLAIM | MB_SYSTEMMODAL)
+        ctypes.windll.user32.MessageBoxA(0, message, title, BUTTON_OK | ICON_EXCLAIM | BEHAVIOR_SYSTEM_MODAL)
 
 
