995bf2cfdefef06a079dbb69d4afa9e24cb566b2
Colin Bounouar <colin.bounouar@external.engie.com>
2017-07-04 18:16:20 +0200

    Add the current file to the path

--- tests/pyxelresttest_com_server.py
+++ tests/pyxelresttest_com_server.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 from pyxelrest import com_server
 
@@ -10,15 +11,18 @@ class PyxelRestTestComServer(unittest.TestCase):
     srv = com_server.PythonServer()
 
     def test_direct(self):
+        self.srv.add_path(os.path.dirname(__file__))
         res = self.srv.direct_call('pyxelresttest_com_server', 'my_function', 'arg', 0)
         self.assertEqual(res, 'arg 0')
 
     def test_thread(self):
+        self.srv.add_path(os.path.dirname(__file__))
         self.srv.thread_call('a', 'pyxelresttest_com_server', 'my_function', 'arg', 0)
         res = self.srv.result('a')
         self.assertEqual(res, 'arg 0')
 
     def test_process(self):
+        self.srv.add_path(os.path.dirname(__file__))
         self.srv.process_call('a', 'pyxelresttest_com_server', 'my_function', 'arg', 0)
         res = self.srv.result('a')
         self.assertEqual(res, 'arg 0')
