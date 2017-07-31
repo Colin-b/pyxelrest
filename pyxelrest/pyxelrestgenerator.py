8498696183573f5845bdbbd2dfb2e8d7d3e8c041
François Merlin <francois.merlin@external.engie.com>
2017-07-31 09:51:11 +0200

    Options to genrate UDF in a different module without flattenizer

--- pyxelrest/pyxelrestgenerator.py
+++ pyxelrest/pyxelrestgenerator.py
@@ -26,7 +26,7 @@ else:
     from imp import reload
 
 
-def user_defined_functions(loaded_services):
+def user_defined_functions(loaded_services, flattenize=True):
     """
     Create xlwings User Defined Functions according to user_defined_functions template.
     :return: A string containing python code with all xlwings UDFs.
@@ -42,7 +42,8 @@ def user_defined_functions(loaded_services):
         modified_parameters={value: key for key, value in vba.vba_restricted_keywords.items()},
         support_pandas=swagger.support_pandas(),
         support_ujson=support_ujson(),
-        authentication=authentication
+        authentication=authentication,
+        flattenize=flattenize
     )
 
 
@@ -54,16 +55,16 @@ def support_ujson():
         return False
 
 
-def generate_user_defined_functions():
+def generate_user_defined_functions(output='user_defined_functions.py', flattenize=True):
     """
     Load services and create user_defined_functions.py python file containing generated xlwings User Defined Functions.
     :return: None
     """
     services = swagger.load_services()
     logging.debug('Generating user defined functions.')
-    with open(os.path.join(os.path.dirname(__file__), 'user_defined_functions.py'), 'w', encoding='utf-8') \
+    with open(os.path.join(os.path.dirname(__file__), output), 'w', encoding='utf-8') \
             as generated_file:
-        generated_file.write(user_defined_functions(services))
+        generated_file.write(user_defined_functions(services, flattenize))
 
 
 def reload_user_defined_functions():
