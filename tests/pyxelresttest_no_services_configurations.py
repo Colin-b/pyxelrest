24f63b764f5344811c28d14e689bb277e81a69ac
Colin-b <colin.bounouar@gmail.com>
2020-01-08 19:19:39 +0100

    Start migration towards pytest

--- tests/pyxelresttest_no_services_configurations.py
+++ tests/pyxelresttest_no_services_configurations.py
@@ -1,17 +1,13 @@
-import unittest
+import pytest
 
 from testsutils import loader
 import pyxelrest
 
 
-class PyxelRestNoServicesConfigurationTest(unittest.TestCase):
-    def test_without_service_configuration_file(self):
-        with self.assertRaises(Exception) as cm:
-            loader.load('non_existing.yml')
-        self.assertEqual(
-            cm.exception.args[0],
-            f'"{pyxelrest.SERVICES_CONFIGURATION_FILE_PATH}" configuration file cannot be read.')
-
-
-if __name__ == '__main__':
-    unittest.main()
+def test_without_service_configuration_file():
+    with pytest.raises(Exception) as exception_info:
+        loader.load("non_existing.yml")
+    assert (
+        str(exception_info.value)
+        == f'"{pyxelrest.SERVICES_CONFIGURATION_FILE_PATH}" configuration file cannot be read.'
+    )
