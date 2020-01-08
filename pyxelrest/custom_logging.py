24bbdf913851d6ecdccde5ce06d0a54f8bd5bd70
Colin-b <colin.bounouar@gmail.com>
2020-01-08 19:02:22 +0100

    Start migration towards pytest

--- pyxelrest/custom_logging.py
+++ pyxelrest/custom_logging.py
@@ -12,7 +12,9 @@ logger = logging.getLogger(__name__)
 
 
 def log_uncaught_exception(exec_type, exex_value, traceback, logger=logging):
-    logger.error("Logging an uncaught exception", exc_info=(exec_type, exex_value, traceback))
+    logger.error(
+        "Logging an uncaught exception", exc_info=(exec_type, exex_value, traceback)
+    )
 
 
 sys.excepthook = log_uncaught_exception
@@ -26,22 +28,33 @@ def load_logging_configuration():
     :return: None
     """
     if os.path.isfile(LOGGING_CONFIGURATION_FILE_PATH):
-        with open(LOGGING_CONFIGURATION_FILE_PATH, 'r') as config_file:
-             log_config_dict = yaml.load(config_file)
-             logging.config.dictConfig(log_config_dict)
-             logger.info(f'Loading PyxelRest: {version.__version__} Python: {sys.version} OS: {platform.platform()} Lib: {sysconfig.get_python_lib()}')
+        with open(LOGGING_CONFIGURATION_FILE_PATH, "r") as config_file:
+            log_config_dict = yaml.load(config_file, Loader=yaml.FullLoader)
+            logging.config.dictConfig(log_config_dict)
+            logger.info(
+                f"Loading PyxelRest: {version.__version__} Python: {sys.version} OS: {platform.platform()} Lib: {sysconfig.get_python_lib()}"
+            )
     else:
-        set_file_logger('pyxelrest')
-        logger.warning(f'Logging configuration file ({LOGGING_CONFIGURATION_FILE_PATH}) cannot be found. Using default logging configuration.')
+        set_file_logger("pyxelrest")
+        logger.warning(
+            f"Logging configuration file ({LOGGING_CONFIGURATION_FILE_PATH}) cannot be found. Using default logging configuration."
+        )
 
 
 def set_file_logger(filename: str, level=logging.INFO):
-    default_log_file_path = os.path.join(os.getenv('APPDATA'), 'pyxelrest', 'logs', filename + '.log')
+    default_log_file_path = os.path.join(
+        os.getenv("APPDATA"), "pyxelrest", "logs", filename + ".log"
+    )
     default_log_folder = os.path.dirname(default_log_file_path)
     if not os.path.exists(default_log_folder):
         os.makedirs(default_log_folder)
     logging.basicConfig(
-        format='%(asctime)s - %(levelname)s - %(process)d:%(thread)d - %(filename)s:%(lineno)d - %(message)s',
-        handlers=[logging.handlers.TimedRotatingFileHandler(default_log_file_path, when='D')],
-        level=level)
-    logger.info(f'Loading PyxelRest: {version.__version__} Python: {sys.version} OS: {platform.platform()} Lib: {sysconfig.get_python_lib()}')
+        format="%(asctime)s - %(levelname)s - %(process)d:%(thread)d - %(filename)s:%(lineno)d - %(message)s",
+        handlers=[
+            logging.handlers.TimedRotatingFileHandler(default_log_file_path, when="D")
+        ],
+        level=level,
+    )
+    logger.info(
+        f"Loading PyxelRest: {version.__version__} Python: {sys.version} OS: {platform.platform()} Lib: {sysconfig.get_python_lib()}"
+    )
--- setup.py
+++ setup.py
@@ -6,37 +6,38 @@ import distutils.sysconfig
 this_dir = os.path.abspath(os.path.dirname(__file__))
 # Corresponds to Lib\site-packages folder
 modules_dir = distutils.sysconfig.get_python_lib()
-data_dir = os.path.join(modules_dir, '..', '..')
-scripts_dir = os.path.join(modules_dir, '..', '..', 'Scripts')
+data_dir = os.path.join(modules_dir, "..", "..")
+scripts_dir = os.path.join(modules_dir, "..", "..", "Scripts")
 
 
 class CustomInstall(install_data):
     def run(self):
         install_data.run(self)
 
-        self.announce('Performing post installation tasks...')
+        self.announce("Performing post installation tasks...")
         from pyxelrest_post_install import PostInstall
+
         post_install = PostInstall(installation_files_folder=this_dir)
         post_install.perform_post_installation_tasks()
 
 
-with open(os.path.join(this_dir, 'README.md'), 'r') as f:
+with open(os.path.join(this_dir, "README.md"), "r") as f:
     long_description = f.read()
 
 # More information on properties: https://packaging.python.org/distributing
 setup(
-    name='pyxelrest',
+    name="pyxelrest",
     version=open("pyxelrest/version.py").readlines()[-1].split()[-1].strip("\"'"),
-    author='Colin Bounouar',
-    author_email='colin.bounouar.dev@gmail.com',
-    maintainer='Colin Bounouar',
-    maintainer_email='colin.bounouar.dev@gmail.com',
+    author="Colin Bounouar",
+    author_email="colin.bounouar.dev@gmail.com",
+    maintainer="Colin Bounouar",
+    maintainer_email="colin.bounouar.dev@gmail.com",
     url="https://colin-b.github.io/pyxelrest/",
     description="Access REST APIs from Excel using User Defined Functions (UDF)",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    download_url='https://pypi.org/project/pyxelrest/',
-    license='MIT',
+    download_url="https://pypi.org/project/pyxelrest/",
+    license="MIT",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
@@ -49,137 +50,121 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Operating System :: Microsoft :: Windows :: Windows 7",
     ],
-    keywords=[
-        'excel',
-        'openapi',
-        'swagger',
-        'rest',
-        'udf',
-        'service',
-    ],
-    packages=find_packages(exclude=['tests*']),
+    keywords=["excel", "openapi", "swagger", "rest", "udf", "service"],
+    packages=find_packages(exclude=["tests*"]),
     package_data={
-        'pyxelrest': [
-            'default_logging_configuration.yml.jinja2',
-            'default_services_configuration.yml',
-            'user_defined_functions.jinja2',
+        "pyxelrest": [
+            "default_logging_configuration.yml.jinja2",
+            "default_services_configuration.yml",
+            "user_defined_functions.jinja2",
         ]
     },
     data_files=[
         (
-            'pyxelrest_addin', [
-                'addin/AutoLoadPyxelRestAddIn/bin/Release/AutoLoadPyxelRestAddIn.dll',
-                'addin/AutoLoadPyxelRestAddIn/bin/Release/AutoLoadPyxelRestAddIn.dll.config',
-                'addin/AutoLoadPyxelRestAddIn/bin/Release/AutoLoadPyxelRestAddIn.dll.manifest',
-                'addin/AutoLoadPyxelRestAddIn/bin/Release/AutoLoadPyxelRestAddIn.vsto',
+            "pyxelrest_addin",
+            [
+                "addin/AutoLoadPyxelRestAddIn/bin/Release/AutoLoadPyxelRestAddIn.dll",
+                "addin/AutoLoadPyxelRestAddIn/bin/Release/AutoLoadPyxelRestAddIn.dll.config",
+                "addin/AutoLoadPyxelRestAddIn/bin/Release/AutoLoadPyxelRestAddIn.dll.manifest",
+                "addin/AutoLoadPyxelRestAddIn/bin/Release/AutoLoadPyxelRestAddIn.vsto",
                 # Package Dependencies to ensure that it will work on any client
-                'addin/AutoLoadPyxelRestAddIn/bin/Release/log4net.dll',
-                'addin/AutoLoadPyxelRestAddIn/bin/Release/log4net.xml',
-                'addin/AutoLoadPyxelRestAddIn/bin/Release/Microsoft.Office.Tools.Common.v4.0.Utilities.dll',
-                'addin/AutoLoadPyxelRestAddIn/bin/Release/Microsoft.Office.Tools.Common.v4.0.Utilities.xml',
-                'addin/AutoLoadPyxelRestAddIn/bin/Release/Microsoft.Office.Tools.Excel.dll',
-                'addin/AutoLoadPyxelRestAddIn/bin/Release/Microsoft.Office.Tools.Excel.xml',
-                'addin/AutoLoadPyxelRestAddIn/bin/Release/Microsoft.Office.Tools.v4.0.Framework.dll',
-                'addin/AutoLoadPyxelRestAddIn/bin/Release/Microsoft.Office.Tools.v4.0.Framework.xml',
-                'addin/AutoLoadPyxelRestAddIn/bin/Release/Microsoft.VisualStudio.Tools.Applications.Runtime.dll',
-                'addin/AutoLoadPyxelRestAddIn/bin/Release/Microsoft.VisualStudio.Tools.Applications.Runtime.xml',
-                'addin/AutoLoadPyxelRestAddIn/bin/Release/YamlDotNet.dll',
-                'addin/AutoLoadPyxelRestAddIn/bin/Release/YamlDotNet.xml',
+                "addin/AutoLoadPyxelRestAddIn/bin/Release/log4net.dll",
+                "addin/AutoLoadPyxelRestAddIn/bin/Release/log4net.xml",
+                "addin/AutoLoadPyxelRestAddIn/bin/Release/Microsoft.Office.Tools.Common.v4.0.Utilities.dll",
+                "addin/AutoLoadPyxelRestAddIn/bin/Release/Microsoft.Office.Tools.Common.v4.0.Utilities.xml",
+                "addin/AutoLoadPyxelRestAddIn/bin/Release/Microsoft.Office.Tools.Excel.dll",
+                "addin/AutoLoadPyxelRestAddIn/bin/Release/Microsoft.Office.Tools.Excel.xml",
+                "addin/AutoLoadPyxelRestAddIn/bin/Release/Microsoft.Office.Tools.v4.0.Framework.dll",
+                "addin/AutoLoadPyxelRestAddIn/bin/Release/Microsoft.Office.Tools.v4.0.Framework.xml",
+                "addin/AutoLoadPyxelRestAddIn/bin/Release/Microsoft.VisualStudio.Tools.Applications.Runtime.dll",
+                "addin/AutoLoadPyxelRestAddIn/bin/Release/Microsoft.VisualStudio.Tools.Applications.Runtime.xml",
+                "addin/AutoLoadPyxelRestAddIn/bin/Release/YamlDotNet.dll",
+                "addin/AutoLoadPyxelRestAddIn/bin/Release/YamlDotNet.xml",
                 # VB Add-in
-                'addin/pyxelrest.xlam',
-            ]
+                "addin/pyxelrest.xlam",
+            ],
         ),
         (
-            'pyxelrest_resources', [
-                'pyxelrest_resources/excel_logo_100.png',
-                'pyxelrest_resources/excel_logo_error_100.png',
-                'pyxelrest_resources/excel_logo_greyscale_100.png',
-                'pyxelrest_resources/python_logo_100.png',
-                'pyxelrest_resources/python_logo_error_100.png',
-                'pyxelrest_resources/python_logo_greyscale_100.png',
-                'pyxelrest_resources/settings_logo_100.png',
-                'pyxelrest_resources/settings_logo_error_100.png',
-                'pyxelrest_resources/settings_logo_greyscale_100.png'
-            ]
+            "pyxelrest_resources",
+            [
+                "pyxelrest_resources/excel_logo_100.png",
+                "pyxelrest_resources/excel_logo_error_100.png",
+                "pyxelrest_resources/excel_logo_greyscale_100.png",
+                "pyxelrest_resources/python_logo_100.png",
+                "pyxelrest_resources/python_logo_error_100.png",
+                "pyxelrest_resources/python_logo_greyscale_100.png",
+                "pyxelrest_resources/settings_logo_100.png",
+                "pyxelrest_resources/settings_logo_error_100.png",
+                "pyxelrest_resources/settings_logo_greyscale_100.png",
+            ],
         ),
         (
-            'pyxelrest_addin/resources', [
-                'addin/AutoLoadPyxelRestAddIn/bin/Release/resources/add-file-16.png',
-                'addin/AutoLoadPyxelRestAddIn/bin/Release/resources/data-transfer-download-128.png',
-                'addin/AutoLoadPyxelRestAddIn/bin/Release/resources/file-4-16.png',
-                'addin/AutoLoadPyxelRestAddIn/bin/Release/resources/folder-3-128.png',
-                'addin/AutoLoadPyxelRestAddIn/bin/Release/resources/plus-4-16.png',
-                'addin/AutoLoadPyxelRestAddIn/bin/Release/resources/plus-4-16_grey.png',
-                'addin/AutoLoadPyxelRestAddIn/bin/Release/resources/plus-5-16.png',
-                'addin/AutoLoadPyxelRestAddIn/bin/Release/resources/refresh-128.png',
-                'addin/AutoLoadPyxelRestAddIn/bin/Release/resources/settings-8-16.ico',
-                'addin/AutoLoadPyxelRestAddIn/bin/Release/resources/settings-8-128.png',
-                'addin/AutoLoadPyxelRestAddIn/bin/Release/resources/x-mark-3-16.png',
-                'addin/AutoLoadPyxelRestAddIn/bin/Release/resources/x-mark-4-16.png'
-            ]
+            "pyxelrest_addin/resources",
+            [
+                "addin/AutoLoadPyxelRestAddIn/bin/Release/resources/add-file-16.png",
+                "addin/AutoLoadPyxelRestAddIn/bin/Release/resources/data-transfer-download-128.png",
+                "addin/AutoLoadPyxelRestAddIn/bin/Release/resources/file-4-16.png",
+                "addin/AutoLoadPyxelRestAddIn/bin/Release/resources/folder-3-128.png",
+                "addin/AutoLoadPyxelRestAddIn/bin/Release/resources/plus-4-16.png",
+                "addin/AutoLoadPyxelRestAddIn/bin/Release/resources/plus-4-16_grey.png",
+                "addin/AutoLoadPyxelRestAddIn/bin/Release/resources/plus-5-16.png",
+                "addin/AutoLoadPyxelRestAddIn/bin/Release/resources/refresh-128.png",
+                "addin/AutoLoadPyxelRestAddIn/bin/Release/resources/settings-8-16.ico",
+                "addin/AutoLoadPyxelRestAddIn/bin/Release/resources/settings-8-128.png",
+                "addin/AutoLoadPyxelRestAddIn/bin/Release/resources/x-mark-3-16.png",
+                "addin/AutoLoadPyxelRestAddIn/bin/Release/resources/x-mark-4-16.png",
+            ],
         ),
     ],
     install_requires=[
         # Used to generate UDFs python file from a template
-        'jinja2==2.*',
+        "jinja2==2.*",
         # Used to communicate with services
-        'requests==2.*',
+        "requests==2.*",
         # Used to communicate with Microsoft Excel (pywin32 dependency is also used to check that Excel is not running)
-        'xlwings==0.17.*',
+        "xlwings==0.17.*",
         # Used to parse configuration files
-        'pyyaml==5.*',
-        'pyaml==19.*',
+        "pyyaml==5.*",
+        "pyaml==19.*",
         # Used to manage authentication
-        'requests_auth==5.*',
+        "requests_auth==5.*",
         # Used to parse all date-time formats in a easy way
-        'python-dateutil==2.*',
+        "python-dateutil==2.*",
     ],
     extra_requires={
         # Support for `application/msgpackpandas`
-        'pandas_msgpack': [
-            'pandas==0.25.*',
-            'msgpack-python==0.5.*',
-        ],
+        "pandas_msgpack": ["pandas==0.25.*", "msgpack-python==0.5.*"],
         # Support for faster JSON serialization / deserialization
-        'ujson': [
-            'ujson==1.*',
-        ],
+        "ujson": ["ujson==1.*"],
         # Support for NTLM authentication
-        'ntlm': [
-            'requests_ntlm==1.*',
-            'requests_negotiate_sspi==0.5.*',
-        ],
+        "ntlm": ["requests_ntlm==1.*", "requests_negotiate_sspi==0.5.*"],
         # Support for in-memory caching
-        'cachetools': [
-            'cachetools==4.*',
-        ],
+        "cachetools": ["cachetools==4.*"],
         "testing": [
             # Used to run tests
-            'nose==1.*',
+            "pytest==5.*",
+            # Used to mock responses
+            "pytest-responses==0.4.*",
             # Used to generate a jwt token
-            'pyjwt==1.*',
+            "pyjwt==1.*",
             # used for testing cache of results
-            'cachetools==4.*',
+            "cachetools==4.*",
             # Used to create test services
-            'flask==1.*',
+            "flask==1.*",
         ],
     },
-    python_requires='>=3.6',
+    python_requires=">=3.6",
     scripts=[
-        'pyxelrest_auto_update.py',
-        'pyxelrest_post_install.py',
-        'pyxelrest_install_addin.py',
-        'pyxelrest_update_services_config.py',
+        "pyxelrest_auto_update.py",
+        "pyxelrest_post_install.py",
+        "pyxelrest_install_addin.py",
+        "pyxelrest_update_services_config.py",
     ],
     project_urls={
         "GitHub": "https://github.com/Colin-b/pyxelrest",
         "Changelog": "https://github.com/Colin-b/pyxelrest/blob/master/CHANGELOG.md",
         "Issues": "https://github.com/Colin-b/pyxelrest/issues",
     },
-    platforms=[
-        'Windows',
-    ],
-    cmdclass={
-        'install_data': CustomInstall,
-    }
+    platforms=["Windows"],
+    cmdclass={"install_data": CustomInstall},
 )
