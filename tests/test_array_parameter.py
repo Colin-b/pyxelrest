b6c1d8df3198a0f860c1864d90334e63630e28ff
Colin-b <colin.bounouar@gmail.com>
2020-01-29 12:38:48 +0100

    Move array parameter related tests into a specific file

---
+++ tests/test_array_parameter.py
@@ -0,0 +1,276 @@
+import pytest
+from requests import PreparedRequest
+from responses import RequestsMock
+
+from testsutils import loader
+
+
+def _get_request(responses: RequestsMock, url: str) -> PreparedRequest:
+    for call in responses.calls:
+        if call.request.url == url:
+            # Pop out verified request (to be able to check multiple requests)
+            responses.calls._calls.remove(call)
+            return call.request
+
+
+@pytest.fixture
+def array_parameter_service(responses: RequestsMock):
+    responses.add(
+        responses.GET,
+        url="http://localhost:8953/",
+        json={
+            "swagger": "2.0",
+            "definitions": {
+                "TestObject": {
+                    "type": "object",
+                    "properties": {"test": {"type": "string", "description": "test"}},
+                    "title": "Test",
+                }
+            },
+            "paths": {
+                "/string_multi_array_parameter": {
+                    "get": {
+                        "operationId": "get_string_multi_array_parameter",
+                        "parameters": [
+                            {
+                                "description": "string array parameter",
+                                "in": "query",
+                                "name": "string_array",
+                                "required": True,
+                                "items": {"type": "string"},
+                                "type": "array",
+                                "collectionFormat": "multi",
+                            }
+                        ],
+                        "responses": {
+                            200: {
+                                "description": "successful operation",
+                                "schema": {
+                                    "items": {"$ref": "#/definitions/TestObject"},
+                                    "type": "array",
+                                },
+                            }
+                        },
+                    }
+                },
+                "/string_default_array_parameter": {
+                    "get": {
+                        "operationId": "get_string_default_array_parameter",
+                        "parameters": [
+                            {
+                                "description": "string array parameter",
+                                "in": "query",
+                                "name": "string_array",
+                                "required": True,
+                                "items": {"type": "string"},
+                                "type": "array",
+                            }
+                        ],
+                        "responses": {
+                            200: {
+                                "description": "successful operation",
+                                "schema": {
+                                    "items": {"$ref": "#/definitions/TestObject"},
+                                    "type": "array",
+                                },
+                            }
+                        },
+                    }
+                },
+                "/string_csv_array_parameter": {
+                    "get": {
+                        "operationId": "get_string_csv_array_parameter",
+                        "parameters": [
+                            {
+                                "description": "string array parameter",
+                                "in": "query",
+                                "name": "string_array",
+                                "required": True,
+                                "items": {"type": "string"},
+                                "type": "array",
+                                "collectionFormat": "csv",
+                            }
+                        ],
+                        "responses": {
+                            200: {
+                                "description": "successful operation",
+                                "schema": {
+                                    "items": {"$ref": "#/definitions/TestObject"},
+                                    "type": "array",
+                                },
+                            }
+                        },
+                    }
+                },
+                "/string_ssv_array_parameter": {
+                    "get": {
+                        "operationId": "get_string_ssv_array_parameter",
+                        "parameters": [
+                            {
+                                "description": "string array parameter",
+                                "in": "query",
+                                "name": "string_array",
+                                "required": True,
+                                "items": {"type": "string"},
+                                "type": "array",
+                                "collectionFormat": "ssv",
+                            }
+                        ],
+                        "responses": {
+                            200: {
+                                "description": "successful operation",
+                                "schema": {
+                                    "items": {"$ref": "#/definitions/TestObject"},
+                                    "type": "array",
+                                },
+                            }
+                        },
+                    }
+                },
+                "/string_tsv_array_parameter": {
+                    "get": {
+                        "operationId": "get_string_tsv_array_parameter",
+                        "parameters": [
+                            {
+                                "description": "string array parameter",
+                                "in": "query",
+                                "name": "string_array",
+                                "required": True,
+                                "items": {"type": "string"},
+                                "type": "array",
+                                "collectionFormat": "tsv",
+                            }
+                        ],
+                        "responses": {
+                            200: {
+                                "description": "successful operation",
+                                "schema": {
+                                    "items": {"$ref": "#/definitions/TestObject"},
+                                    "type": "array",
+                                },
+                            }
+                        },
+                    }
+                },
+                "/string_pipes_array_parameter": {
+                    "get": {
+                        "operationId": "get_string_pipes_array_parameter",
+                        "parameters": [
+                            {
+                                "description": "string array parameter",
+                                "in": "query",
+                                "name": "string_array",
+                                "required": True,
+                                "items": {"type": "string"},
+                                "type": "array",
+                                "collectionFormat": "pipes",
+                            }
+                        ],
+                        "responses": {
+                            200: {
+                                "description": "successful operation",
+                                "schema": {
+                                    "items": {"$ref": "#/definitions/TestObject"},
+                                    "type": "array",
+                                },
+                            }
+                        },
+                    }
+                },
+            },
+        },
+        match_querystring=True,
+    )
+    loader.load("array_parameter_service.yml")
+
+
+def test_string_multi_array_parameter(responses: RequestsMock, array_parameter_service):
+    responses.add(
+        responses.GET,
+        url="http://localhost:8953/string_multi_array_parameter?string_array=str1&string_array=str2",
+        json={},
+        match_querystring=True,
+    )
+
+    from pyxelrest import pyxelrestgenerator
+
+    assert pyxelrestgenerator.array_parameter_get_string_multi_array_parameter(
+        ["str1", "str2"]
+    ) == [[""]]
+
+
+def test_string_default_array_parameter(
+    responses: RequestsMock, array_parameter_service
+):
+    responses.add(
+        responses.GET,
+        url="http://localhost:8953/string_default_array_parameter?string_array=str1,str2",
+        json={},
+        match_querystring=True,
+    )
+
+    from pyxelrest import pyxelrestgenerator
+
+    assert pyxelrestgenerator.array_parameter_get_string_default_array_parameter(
+        ["str1", "str2"]
+    ) == [[""]]
+
+
+def test_string_csv_array_parameter(responses: RequestsMock, array_parameter_service):
+    responses.add(
+        responses.GET,
+        url="http://localhost:8953/string_csv_array_parameter?string_array=str1,str2",
+        json={},
+        match_querystring=True,
+    )
+
+    from pyxelrest import pyxelrestgenerator
+
+    assert pyxelrestgenerator.array_parameter_get_string_csv_array_parameter(
+        ["str1", "str2"]
+    ) == [[""]]
+
+
+def test_string_ssv_array_parameter(responses: RequestsMock, array_parameter_service):
+    responses.add(
+        responses.GET,
+        url="http://localhost:8953/string_ssv_array_parameter?string_array=str1 str2",
+        json={},
+        match_querystring=True,
+    )
+
+    from pyxelrest import pyxelrestgenerator
+
+    assert pyxelrestgenerator.array_parameter_get_string_ssv_array_parameter(
+        ["str1", "str2"]
+    ) == [[""]]
+
+
+def test_string_tsv_array_parameter(responses: RequestsMock, array_parameter_service):
+    responses.add(
+        responses.GET,
+        url="http://localhost:8953/string_tsv_array_parameter?string_array=str1\tstr2",
+        json={},
+        match_querystring=True,
+    )
+
+    from pyxelrest import pyxelrestgenerator
+
+    assert pyxelrestgenerator.array_parameter_get_string_tsv_array_parameter(
+        ["str1", "str2"]
+    ) == [[""]]
+
+
+def test_string_pipes_array_parameter(responses: RequestsMock, array_parameter_service):
+    responses.add(
+        responses.GET,
+        url="http://localhost:8953/string_pipes_array_parameter?string_array=str1|str2",
+        json={},
+        match_querystring=True,
+    )
+
+    from pyxelrest import pyxelrestgenerator
+
+    assert pyxelrestgenerator.array_parameter_get_string_pipes_array_parameter(
+        ["str1", "str2"]
+    ) == [[""]]
