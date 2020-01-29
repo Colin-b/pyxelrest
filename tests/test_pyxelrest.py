38dfc0f67a754809dd95313a8f88f74d69336589
Colin-b <colin.bounouar@gmail.com>
2020-01-29 15:36:19 +0100

    Move without parameter related tests into a specific file

--- tests/test_pyxelrest.py
+++ tests/test_pyxelrest.py
@@ -2971,139 +2971,6 @@ def open_api_definition_parsing_service(responses: RequestsMock):
 
 
 @pytest.fixture
-def without_parameter_service(responses: RequestsMock):
-    responses.add(
-        responses.GET,
-        url="http://localhost:8950/",
-        json={
-            "swagger": "2.0",
-            "definitions": {"Test": {"properties": {}}},
-            "paths": {
-                "/without_parameter": {
-                    "get": {
-                        "operationId": "get_without_parameter",
-                        "responses": {
-                            "200": {
-                                "description": "return value",
-                                "schema": {"type": "string"},
-                            }
-                        },
-                    },
-                    "post": {
-                        "operationId": "post_without_parameter",
-                        "responses": {
-                            "200": {"description": "POST performed properly"}
-                        },
-                    },
-                    "put": {
-                        "operationId": "put_without_parameter",
-                        "responses": {"200": {"description": "PUT performed properly"}},
-                    },
-                    "delete": {
-                        "operationId": "delete_without_parameter",
-                        "responses": {
-                            "200": {"description": "DELETE performed properly"}
-                        },
-                    },
-                },
-                "/plain_text_without_parameter": {
-                    "get": {
-                        "operationId": "get_plain_text_without_parameter",
-                        "produces": ["text/plain"],
-                        "responses": {
-                            "200": {
-                                "description": "return value",
-                                "schema": {"type": "string"},
-                            }
-                        },
-                    },
-                    "post": {
-                        "operationId": "post_plain_text_without_parameter",
-                        "produces": ["text/plain"],
-                        "responses": {"200": {"description": "return value"}},
-                    },
-                    "put": {
-                        "operationId": "put_plain_text_without_parameter",
-                        "produces": ["text/plain"],
-                        "responses": {"200": {"description": "return value"}},
-                    },
-                    "delete": {
-                        "operationId": "delete_plain_text_without_parameter",
-                        "produces": ["text/plain"],
-                        "responses": {"200": {"description": "return value"}},
-                    },
-                },
-                "/json_without_parameter": {
-                    "get": {
-                        "operationId": "get_json_without_parameter",
-                        "produces": ["application/json"],
-                        "responses": {
-                            "200": {
-                                "description": "return value",
-                                "$ref": "#/definitions/Test",
-                            }
-                        },
-                    },
-                    "post": {
-                        "operationId": "post_json_without_parameter",
-                        "produces": ["application/json"],
-                        "responses": {
-                            "200": {
-                                "description": "return value",
-                                "$ref": "#/definitions/Test",
-                            }
-                        },
-                    },
-                    "put": {
-                        "operationId": "put_json_without_parameter",
-                        "produces": ["application/json"],
-                        "responses": {
-                            "200": {
-                                "description": "return value",
-                                "$ref": "#/definitions/Test",
-                            }
-                        },
-                    },
-                    "delete": {
-                        "operationId": "delete_json_without_parameter",
-                        "produces": ["application/json"],
-                        "responses": {
-                            "200": {
-                                "description": "return value",
-                                "$ref": "#/definitions/Test",
-                            }
-                        },
-                    },
-                },
-                "/octet_without_parameter": {
-                    "get": {
-                        "operationId": "get_octet_without_parameter",
-                        "produces": ["application/octet-stream"],
-                        "responses": {"200": {"description": "return value"}},
-                    },
-                    "post": {
-                        "operationId": "post_octet_without_parameter",
-                        "produces": ["application/octet-stream"],
-                        "responses": {"200": {"description": "return value"}},
-                    },
-                    "put": {
-                        "operationId": "put_octet_without_parameter",
-                        "produces": ["application/octet-stream"],
-                        "responses": {"200": {"description": "return value"}},
-                    },
-                    "delete": {
-                        "operationId": "delete_octet_without_parameter",
-                        "produces": ["application/octet-stream"],
-                        "responses": {"200": {"description": "return value"}},
-                    },
-                },
-            },
-        },
-        match_querystring=True,
-    )
-
-
-@pytest.fixture
 def header_parameter_service(responses: RequestsMock):
     responses.add(
         responses.GET,
@@ -3335,13 +3202,11 @@ def async_service(responses: RequestsMock):
 
 @pytest.fixture
 def services(
-    responses: RequestsMock,
     usual_parameters_service,
     filtered_tags_service,
     values_false_service,
     output_order_service,
     open_api_definition_parsing_service,
-    without_parameter_service,
     header_parameter_service,
     form_parameter_service,
     http_methods_service,
@@ -3351,53 +3216,6 @@ def services(
 ):
     # TODO add static_file_call_service mock to the specific test case
     loader.load("services.yml")
-
-
-def test_plain_without_parameter(responses: RequestsMock, services):
-    from pyxelrest import pyxelrestgenerator
-
-    assert (
-        pyxelrestgenerator.without_parameter_get_plain_text_without_parameter()
-        == "string value returned should be truncated so that the following information cannot be seen by user, because of the fact that Excel does not allow more than 255 characters in a cell. Only the 255 characters will be returned by the user defined functions:  "
-    )
-
-
-def test_post_without_parameter(responses: RequestsMock, services):
-    from pyxelrest import pyxelrestgenerator
-
-    assert (
-        pyxelrestgenerator.without_parameter_post_without_parameter()
-        == "POST performed properly"
-    )
-
-
-def test_put_without_parameter(responses: RequestsMock, services):
-    from pyxelrest import pyxelrestgenerator
-
-    assert (
-        pyxelrestgenerator.without_parameter_put_without_parameter()
-        == "PUT performed properly"
-    )
-
-
-def test_delete_without_parameter(responses: RequestsMock, services):
-    from pyxelrest import pyxelrestgenerator
-
-    assert (
-        pyxelrestgenerator.without_parameter_delete_without_parameter()
-        == "DELETE performed properly"
-    )
-
-
-def test_service_without_sync_does_not_have_sync(responses: RequestsMock, services):
-    from pyxelrest import pyxelrestgenerator
-
-    with pytest.raises(AttributeError) as exception_info:
-        pyxelrestgenerator.vba_without_parameter_delete_without_parameter()
-    assert (
-        str(exception_info.value)
-        == "module 'pyxelrest.pyxelrestgenerator' has no attribute 'vba_without_parameter_delete_without_parameter'"
-    )
 
 
 def test_get_header_parameter(responses: RequestsMock, services):
