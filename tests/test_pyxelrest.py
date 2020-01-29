2b3dbe4b3150e272fd58cd111fbd7b3bf8c0ab67
Colin-b <colin.bounouar@gmail.com>
2020-01-29 18:30:04 +0100

    Move output order tests into a dedicated test file

--- tests/test_pyxelrest.py
+++ tests/test_pyxelrest.py
@@ -1,5 +1,3 @@
-import datetime
-
 import pytest
 from requests import PreparedRequest
 from responses import RequestsMock
@@ -22,95 +20,6 @@ def support_pandas():
         return True
     except:
         return False
-
-
-@pytest.fixture
-def output_order_service(responses: RequestsMock):
-    responses.add(
-        responses.GET,
-        url="http://localhost:8946/",
-        json={
-            "swagger": "2.0",
-            "definitions": {
-                "Price": {
-                    "required": ["curve", "date", "mat", "ts"],
-                    "type": "object",
-                    "properties": {
-                        "ts": {
-                            "type": "string",
-                            "description": "timeslot",
-                            "maxLength": 2,
-                        },
-                        "date": {
-                            "type": "string",
-                            "description": "date",
-                            "format": "date",
-                        },
-                        "curve": {
-                            "type": "string",
-                            "description": "curvename",
-                            "maxLength": 20,
-                        },
-                        "mat": {
-                            "type": "string",
-                            "description": "maturity",
-                            "maxLength": 4,
-                        },
-                    },
-                    "title": "RealizedPrice",
-                }
-            },
-            "paths": {
-                "/price/unordered": {
-                    "get": {
-                        "operationId": "get_price_unordered",
-                        "description": "Price",
-                        "parameters": [
-                            {
-                                "in": "query",
-                                "description": "date",
-                                "format": "date",
-                                "required": False,
-                                "type": "string",
-                                "name": "date",
-                            },
-                            {
-                                "name": "curve",
-                                "in": "query",
-                                "required": False,
-                                "type": "string",
-                                "description": "curvename",
-                            },
-                            {
-                                "name": "ts",
-                                "in": "query",
-                                "required": False,
-                                "type": "string",
-                                "description": "timeslot",
-                            },
-                            {
-                                "name": "mat",
-                                "in": "query",
-                                "required": False,
-                                "type": "string",
-                                "description": "maturity",
-                            },
-                        ],
-                        "responses": {
-                            "200": {
-                                "description": "successful operation",
-                                "schema": {
-                                    "items": {"$ref": "#/definitions/Price"},
-                                    "type": "array",
-                                },
-                            }
-                        },
-                    }
-                }
-            },
-        },
-        match_querystring=True,
-    )
 
 
 @pytest.fixture
@@ -278,7 +187,6 @@ def async_service(responses: RequestsMock):
 
 @pytest.fixture
 def services(
-    output_order_service,
     open_api_definition_parsing_service,
     content_type_service,
     base_path_ending_with_slash_service,
@@ -286,17 +194,6 @@ def services(
 ):
     # TODO add static_file_call_service mock to the specific test case
     loader.load("services.yml")
-
-
-def test_get_compare_output_order(responses: RequestsMock, services):
-    from pyxelrest import pyxelrestgenerator
-
-    assert pyxelrestgenerator.output_order_get_price_unordered() == [
-        [u"curve", u"date", u"mat", u"ts"],
-        [u"PW_FR", datetime.datetime(2017, 4, 5, 0, 0), u"H01", u""],
-        [u"PW_FR", datetime.datetime(2017, 4, 5, 0, 0), u"H02", u"2017-04-05 12:03:15"],
-        [u"PW_FR", datetime.datetime(2017, 4, 5, 0, 0), u"H03", u""],
-    ]
 
 
 def test_get_static_open_api_definition(responses: RequestsMock, services):
