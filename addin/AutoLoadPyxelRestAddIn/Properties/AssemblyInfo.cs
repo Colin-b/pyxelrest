696777a49321d12f9a28c4ae1d405ec7c459b2e9
Colin-b <colin.bounouar@gmail.com>
2020-03-04 17:36:08 +0100

    Update addin version to 1.0.0.0

--- addin/AutoLoadPyxelRestAddIn/Properties/AssemblyInfo.cs
+++ addin/AutoLoadPyxelRestAddIn/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Security;
@@ -9,7 +9,7 @@ using System.Security;
 [assembly: AssemblyTitle("AutoLoadPyxelRestAddIn")]
 [assembly: AssemblyDescription("Auto load PyxelRest within Excel.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Engie")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AutoLoadPyxelRestAddIn")]
 [assembly: AssemblyCopyright("MIT License")]
 [assembly: AssemblyTrademark("")]
@@ -33,5 +33,5 @@ using System.Security;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.69.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
 
