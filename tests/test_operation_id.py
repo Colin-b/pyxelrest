e302876afb5225b2af9f81f83c71010870144260
Colin-b <colin.bounouar@gmail.com>
2020-03-09 14:27:13 +0100

    Remove fixture for operation id testing

--- tests/test_operation_id.py
+++ tests/test_operation_id.py
@@ -1,14 +1,12 @@
-import pytest
 from responses import RequestsMock
 
 from tests import loader
 
 
-@pytest.fixture
-def operation_id_services(responses: RequestsMock):
+def test_missing_operation_id(responses: RequestsMock, tmpdir):
     responses.add(
         responses.GET,
-        url="http://localhost:8948/operation_id_not_provided",
+        url="http://test",
         json={
             "swagger": "2.0",
             "paths": {
@@ -19,9 +17,31 @@ def operation_id_services(responses: RequestsMock):
         },
         match_querystring=True,
     )
+    generated_functions = loader.load(
+        tmpdir,
+        {
+            "operation_id_not_provided": {
+                "open_api": {"definition": "http://test"},
+                "udf": {"return_types": ["sync_auto_expand"], "shift_result": False},
+            }
+        },
+    )
     responses.add(
         responses.GET,
-        url="http://localhost:8948/operation_id_not_always_provided",
+        url="http://test/without_operationId",
+        json={},
+        match_querystring=True,
+    )
+
+    assert generated_functions.operation_id_not_provided_get_without_operationId() == [
+        [""]
+    ]
+
+
+def test_mixed_operation_id(responses: RequestsMock, tmpdir):
+    responses.add(
+        responses.GET,
+        url="http://test",
         json={
             "swagger": "2.0",
             "paths": {
@@ -39,59 +59,18 @@ def operation_id_services(responses: RequestsMock):
         },
         match_querystring=True,
     )
-
-
-def test_missing_operation_id(responses: RequestsMock, operation_id_services, tmpdir):
     generated_functions = loader.load(
         tmpdir,
         {
-            "operation_id_not_provided": {
-                "open_api": {
-                    "definition": "http://localhost:8948/operation_id_not_provided"
-                },
-                "udf": {"return_types": ["sync_auto_expand"], "shift_result": False},
-            },
             "operation_id_not_always_provided": {
-                "open_api": {
-                    "definition": "http://localhost:8948/operation_id_not_always_provided"
-                },
+                "open_api": {"definition": "http://test"},
                 "udf": {"return_types": ["sync_auto_expand"], "shift_result": False},
-            },
+            }
         },
     )
     responses.add(
         responses.GET,
-        url="http://localhost:8948/without_operationId",
-        json={},
-        match_querystring=True,
-    )
-
-    assert generated_functions.operation_id_not_provided_get_without_operationId() == [
-        [""]
-    ]
-
-
-def test_mixed_operation_id(responses: RequestsMock, operation_id_services, tmpdir):
-    generated_functions = loader.load(
-        tmpdir,
-        {
-            "operation_id_not_provided": {
-                "open_api": {
-                    "definition": "http://localhost:8948/operation_id_not_provided"
-                },
-                "udf": {"return_types": ["sync_auto_expand"], "shift_result": False},
-            },
-            "operation_id_not_always_provided": {
-                "open_api": {
-                    "definition": "http://localhost:8948/operation_id_not_always_provided"
-                },
-                "udf": {"return_types": ["sync_auto_expand"], "shift_result": False},
-            },
-        },
-    )
-    responses.add(
-        responses.GET,
-        url="http://localhost:8948/without_operationId",
+        url="http://test/without_operationId",
         json=["first"],
         match_querystring=True,
     )
@@ -102,7 +81,7 @@ def test_mixed_operation_id(responses: RequestsMock, operation_id_services, tmpd
 
     responses.add(
         responses.GET,
-        url="http://localhost:8948/with_operationId",
+        url="http://test/with_operationId",
         json=["second"],
         match_querystring=True,
     )
