901dc183e57295acc6ae701a8f3786ccb87720e6
Colin-b <colin.bounouar@gmail.com>
2020-03-10 11:06:42 +0100

    Check if test order matter

--- tests/test_array_parameter.py
+++ tests/test_array_parameter.py
@@ -116,63 +116,6 @@ def test_string_default_array_parameter(responses: RequestsMock, tmpdir):
     ) == [[""]]
 
 
-def test_string_csv_array_parameter(responses: RequestsMock, tmpdir):
-    responses.add(
-        responses.GET,
-        url="http://test/",
-        json={
-            "swagger": "2.0",
-            "paths": {
-                "/test": {
-                    "get": {
-                        "operationId": "get_string_csv_array_parameter",
-                        "parameters": [
-                            {
-                                "description": "string array parameter",
-                                "in": "query",
-                                "name": "string_array",
-                                "required": True,
-                                "items": {"type": "string"},
-                                "type": "array",
-                                "collectionFormat": "csv",
-                            }
-                        ],
-                        "responses": {
-                            200: {
-                                "description": "successful operation",
-                                "schema": {
-                                    "items": {"type": "string"},
-                                    "type": "array",
-                                },
-                            }
-                        },
-                    }
-                }
-            },
-        },
-        match_querystring=True,
-    )
-    generated_functions = loader.load(
-        tmpdir,
-        {
-            "array_parameter": {
-                "open_api": {"definition": "http://test/"},
-                "udf": {"return_types": ["sync_auto_expand"], "shift_result": False},
-            }
-        },
-    )
-    responses.add(
-        responses.GET,
-        url="http://test/test?string_array=str1,str2",
-        json=[],
-        match_querystring=True,
-    )
-
-    assert generated_functions.array_parameter_get_string_csv_array_parameter(
-        ["str1", "str2"]
-    ) == [[""]]
-
-
 def test_string_ssv_array_parameter(responses: RequestsMock, tmpdir):
     responses.add(
         responses.GET,
@@ -286,6 +229,63 @@ def test_string_tsv_array_parameter(responses: RequestsMock, tmpdir):
     )
 
     assert generated_functions.array_parameter_get_string_tsv_array_parameter(
+        ["str1", "str2"]
+    ) == [[""]]
+
+
+def test_string_csv_array_parameter(responses: RequestsMock, tmpdir):
+    responses.add(
+        responses.GET,
+        url="http://test/",
+        json={
+            "swagger": "2.0",
+            "paths": {
+                "/test": {
+                    "get": {
+                        "operationId": "get_string_csv_array_parameter",
+                        "parameters": [
+                            {
+                                "description": "string array parameter",
+                                "in": "query",
+                                "name": "string_array",
+                                "required": True,
+                                "items": {"type": "string"},
+                                "type": "array",
+                                "collectionFormat": "csv",
+                            }
+                        ],
+                        "responses": {
+                            200: {
+                                "description": "successful operation",
+                                "schema": {
+                                    "items": {"type": "string"},
+                                    "type": "array",
+                                },
+                            }
+                        },
+                    }
+                }
+            },
+        },
+        match_querystring=True,
+    )
+    generated_functions = loader.load(
+        tmpdir,
+        {
+            "array_parameter": {
+                "open_api": {"definition": "http://test/"},
+                "udf": {"return_types": ["sync_auto_expand"], "shift_result": False},
+            }
+        },
+    )
+    responses.add(
+        responses.GET,
+        url="http://test/test?string_array=str1,str2",
+        json=[],
+        match_querystring=True,
+    )
+
+    assert generated_functions.array_parameter_get_string_csv_array_parameter(
         ["str1", "str2"]
     ) == [[""]]
 
