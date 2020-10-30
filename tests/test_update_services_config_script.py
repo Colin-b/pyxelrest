2496b0f7ce287d060ba9f4236df4b5baaa7507f1
Colin-b <Colin-b@users.noreply.github.com>
2020-10-30 20:37:31 +0100

    Increase test coverage of services updater

--- tests/test_update_services_config_script.py
+++ tests/test_update_services_config_script.py
@@ -30,3 +30,76 @@ def test_add_one_service_from_file_to_empty_config(tmpdir):
     with open(os.path.join(tmpdir, "test_config.yml")) as test_config:
         assert yaml.load(test_config, Loader=yaml.SafeLoader) == {"svc2": {}}
 
+
+def test_add_all_services_from_directory_to_empty_config(tmpdir):
+    os.makedirs(os.path.join(tmpdir, "services"))
+    with open(os.path.join(tmpdir, "services", "service1.yml"), "w") as service1:
+        yaml.dump({"svc1": {}}, service1)
+    with open(os.path.join(tmpdir, "services", "service2.yml"), "w") as service2:
+        yaml.dump({"svc2": {}}, service2)
+
+    updater = pyxelrest.update_services_config.ServicesConfigUpdater("add", os.path.join(tmpdir, "test_config.yml"))
+    updater.update_configuration(
+        os.path.join(tmpdir, "services")
+    )
+
+    with open(os.path.join(tmpdir, "test_config.yml")) as test_config:
+        assert yaml.load(test_config, Loader=yaml.SafeLoader) == {"svc1": {}, "svc2": {}}
+
+
+def test_add_a_subset_of_services_from_directory_to_empty_config(tmpdir):
+    os.makedirs(os.path.join(tmpdir, "services"))
+    with open(os.path.join(tmpdir, "services", "service1.yml"), "w") as service1:
+        yaml.dump({"svc1": {}}, service1)
+    with open(os.path.join(tmpdir, "services", "service2.yml"), "w") as service2:
+        yaml.dump({"svc2": {}}, service2)
+    with open(os.path.join(tmpdir, "services", "service3.yml"), "w") as service3:
+        yaml.dump({"svc3": {}}, service3)
+
+    updater = pyxelrest.update_services_config.ServicesConfigUpdater("add", os.path.join(tmpdir, "test_config.yml"))
+    updater.update_configuration(
+        os.path.join(tmpdir, "services"), services=["svc2", "svc3"]
+    )
+
+    with open(os.path.join(tmpdir, "test_config.yml")) as test_config:
+        assert yaml.load(test_config, Loader=yaml.SafeLoader) == {"svc2": {}, "svc3": {}}
+
+
+def test_list_services_from_directory(tmpdir, capsys):
+    os.makedirs(os.path.join(tmpdir, "services"))
+    with open(os.path.join(tmpdir, "services", "service1.yml"), "w") as service1:
+        yaml.dump({"svc1": {}}, service1)
+    with open(os.path.join(tmpdir, "services", "service2.yml"), "w") as service2:
+        yaml.dump({"svc2": {}}, service2)
+    with open(os.path.join(tmpdir, "services", "service3.yml"), "w") as service3:
+        yaml.dump({"svc3": {}}, service3)
+
+    updater = pyxelrest.update_services_config.ServicesConfigUpdater("list", os.path.join(tmpdir, "test_config.yml"))
+    updater.update_configuration(
+        os.path.join(tmpdir, "services")
+    )
+
+    assert capsys.readouterr().out == """svc1
+svc2
+svc3
+"""
+
+
+def test_list_subset_of_services_from_directory(tmpdir, capsys):
+    os.makedirs(os.path.join(tmpdir, "services"))
+    with open(os.path.join(tmpdir, "services", "service1.yml"), "w") as service1:
+        yaml.dump({"svc1": {}}, service1)
+    with open(os.path.join(tmpdir, "services", "service2.yml"), "w") as service2:
+        yaml.dump({"svc2": {"description": "service desc"}}, service2)
+    with open(os.path.join(tmpdir, "services", "service3.yml"), "w") as service3:
+        yaml.dump({"svc3": {}}, service3)
+
+    updater = pyxelrest.update_services_config.ServicesConfigUpdater("list", os.path.join(tmpdir, "test_config.yml"))
+    updater.update_configuration(
+        os.path.join(tmpdir, "services"), services=["svc2", "svc3"]
+    )
+
+    assert capsys.readouterr().out == """svc2 - service desc
+svc3
+"""
+
