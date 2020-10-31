ce29d10c31dfa573e824659a7334c4f8d2160c28
Colin-b <Colin-b@users.noreply.github.com>
2020-10-31 12:10:12 +0100

    Reflect new pyxelrest behavior when configuration cannot be found in test cases

--- tests/test_no_services_configurations.py
+++ tests/test_no_services_configurations.py
@@ -1,8 +1,6 @@
 import os
 from importlib import reload, import_module
 
-import pytest
-
 import pyxelrest
 
 
@@ -10,9 +8,4 @@ def test_without_service_configuration_file(tmpdir):
     pyxelrest.SERVICES_CONFIGURATION_FILE_PATH = os.path.join(
         tmpdir, "non_existing_file.yml"
     )
-    with pytest.raises(Exception) as exception_info:
-        reload(import_module("pyxelrest._generator"))
-    assert (
-        str(exception_info.value)
-        == f'"{os.path.join(tmpdir, "non_existing_file.yml")}" configuration file cannot be read.'
-    )
+    reload(import_module("pyxelrest._generator"))
