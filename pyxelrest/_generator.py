e5597c095073284fba1263ab4adeb6304d3dc802
Colin-b <Colin-b@users.noreply.github.com>
2020-10-31 13:15:23 +0100

    Remove unused import

--- pyxelrest/_generator.py
+++ pyxelrest/_generator.py
@@ -3,7 +3,7 @@ Each time this module is loaded (and GENERATE_UDF_ON_IMPORT is True) it will gen
 """
 import os
 from importlib import reload, import_module
-from typing import List, Union
+from typing import List
 import logging.config
 import logging.handlers
 import datetime
