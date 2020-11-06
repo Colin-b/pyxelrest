3051fd1d3783b3501bf3f97d2dc1eb6424558be5
Colin-b <Colin-b@users.noreply.github.com>
2020-11-06 17:19:07 +0100

    Load module confs location from registry if possible. Do not store tokens in configuration folder

--- pyxelrest/_generator_config.py
+++ pyxelrest/_generator_config.py
@@ -1,11 +1,30 @@
 import os
+import winreg
+from typing import Optional
 
-SERVICES_CONFIGURATION_FILE_PATH = os.path.join(
-    os.getenv("APPDATA"), "pyxelrest", "configuration", "services.yml"
-)
-LOGGING_CONFIGURATION_FILE_PATH = os.path.join(
-    os.getenv("APPDATA"), "pyxelrest", "configuration", "logging.yml"
-)
-TOKEN_CACHE_FILE_PATH = os.path.join(
-    os.getenv("APPDATA"), "pyxelrest", "configuration", "tokens.json"
-)
+
+def get_registry_key(key: str) -> Optional[str]:
+    try:
+        with winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"Software\Microsoft\Windows\CurrentVersion\Uninstall\PyxelRest") as pyxelrest_registry_key:
+            value, _ = winreg.QueryValueEx(pyxelrest_registry_key, key)
+        return value
+    except FileNotFoundError:
+        return
+
+
+SERVICES_CONFIGURATION_FILE_PATH = ""
+LOGGING_CONFIGURATION_FILE_PATH = ""
+TOKEN_CACHE_FILE_PATH = ""
+
+install_location = get_registry_key("InstallLocation") or ""
+
+if install_location:
+    SERVICES_CONFIGURATION_FILE_PATH = os.path.join(
+        install_location, "configuration", "services.yml"
+    )
+    LOGGING_CONFIGURATION_FILE_PATH = os.path.join(
+        install_location, "configuration", "logging.yml"
+    )
+    TOKEN_CACHE_FILE_PATH = os.path.join(
+        install_location, "tokens.json"
+    )
