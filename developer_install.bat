87341c675dc47f7dc6da1f7222811251030940d5
Colin-b <Colin-b@users.noreply.github.com>
2020-11-06 18:15:38 +0100

    Cleaner developer install (no need for choices)

--- developer_install.bat
+++ developer_install.bat
@@ -1,36 +1,6 @@
 @ECHO OFF
 CLS
-ECHO 1.Install
-ECHO 2.Update
-ECHO 3.Update (force reinstall)
-ECHO.
 
-CHOICE /C 123 /M "Enter your choice:"
-
-:: Note - list ERRORLEVELS in decreasing order
-IF ERRORLEVEL 3 GOTO ForceUpdate
-IF ERRORLEVEL 2 GOTO Update
-IF ERRORLEVEL 1 GOTO Install
-
-:ForceUpdate
 python -m pip install . --upgrade --force-reinstall
-python pyxelrest_post_install.py
 SET /P "path_to_conf=Please provide path to the up to date configuration:"
 python pyxelrest/install_addin.py --source addin/PyxelRestAddIn/bin/Release --check_pre_releases --path_to_up_to_date_configuration "%path_to_conf%"
-GOTO End
-
-:Update
-python -m pip install . --upgrade
-python pyxelrest_post_install.py
-SET /P "path_to_conf=Please provide path to the up to date configuration:"
-python pyxelrest/install_addin.py --source addin/PyxelRestAddIn/bin/Release --check_pre_releases --path_to_up_to_date_configuration "%path_to_conf%"
-GOTO End
-
-:Install
-python -m pip install .
-python pyxelrest_post_install.py
-SET /P "path_to_conf=Please provide path to the up to date configuration:"
-python pyxelrest/install_addin.py --source addin/PyxelRestAddIn/bin/Release --check_pre_releases --path_to_up_to_date_configuration "%path_to_conf%"
-GOTO End
-
-:End
