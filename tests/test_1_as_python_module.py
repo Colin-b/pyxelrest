a53c4d46105dcb1215f821f580444d6042af767e
Colin-b <Colin-b@users.noreply.github.com>
2020-12-10 14:26:44 +0100

    Ensure text content is returned as is by module

--- tests/test_1_as_python_module.py
+++ tests/test_1_as_python_module.py
@@ -140,3 +140,42 @@ def test_get_user_by_name(responses: RequestsMock, petstore_service):
         "phone": "0123456789",
         "userStatus": 0,
     }
+
+
+def test_raw_text_response(responses: RequestsMock):
+    import pyxelrest
+
+    responses.add(
+        responses.GET,
+        url="http://test/",
+        json={
+            "swagger": "2.0",
+            "paths": {
+                "/text": {
+                    "get": {
+                        "operationId": "get_text",
+                        "responses": {"200": {"description": "return value"}},
+                    }
+                }
+            },
+        },
+        match_querystring=True,
+    )
+
+    pyxelrest.load(
+        {
+            "text_api": {
+                "open_api": {"definition": "http://test/"},
+            }
+        }
+    )
+    responses.add(
+        responses.GET,
+        url="http://test/text",
+        body=b"This is the content of the response.",
+        match_querystring=True,
+    )
+
+    from pyxelrest.user_defined_functions import text_api
+
+    assert text_api.get_text() == "This is the content of the response."
