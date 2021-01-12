e02dccd9472dae368adf0cc70072e82b4b37646d
Colin-b <Colin-b@users.noreply.github.com>
2021-01-12 16:47:21 +0100

    Ensure single item is handled

--- tests/test_dict_parameter.py
+++ tests/test_dict_parameter.py
@@ -1,7 +1,16 @@
 import pytest
+from requests import PreparedRequest
 from responses import RequestsMock
 
 from tests import loader
+
+
+def _get_request(responses: RequestsMock, url: str) -> PreparedRequest:
+    for call in responses.calls:
+        if call.request.url == url:
+            # Pop out verified request (to be able to check multiple requests)
+            responses.calls._calls.remove(call)
+            return call.request
 
 
 @pytest.fixture
@@ -84,4 +93,29 @@ def test_post_dict_parameter_with_more_than_2_rows(
     assert (
         generated_functions.dict_post_dict([["h1", "h2"], ["v1", "v2"], ["v10", "v20"]])
         == "payload value should contains only two rows. Header and values."
+    )
+
+
+def test_post_dict_parameter_with_a_single_item(
+    json_service, tmpdir, responses: RequestsMock
+):
+    generated_functions = loader.load(
+        tmpdir,
+        {
+            "dict": {
+                "open_api": {"definition": "http://localhost:8954/"},
+                "formulas": {"dynamic_array": {"lock_excel": True}},
+            }
+        },
+    )
+    responses.add(
+        responses.POST,
+        url="http://localhost:8954/dict",
+        json=[],
+        match_querystring=True,
+    )
+    assert generated_functions.dict_post_dict(["header", "value"]) == [[""]]
+    assert (
+        _get_request(responses, "http://localhost:8954/dict").body
+        == b"""{"payload": {"header": "value"}}"""
     )
