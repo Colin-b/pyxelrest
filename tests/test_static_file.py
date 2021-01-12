27108d44977abecc21f4e19502dbbcffcc48eb3f
Colin-b <Colin-b@users.noreply.github.com>
2021-01-12 17:45:22 +0100

    Cleanup file adapter behavior

--- tests/test_static_file.py
+++ tests/test_static_file.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 from responses import RequestsMock
@@ -51,6 +52,66 @@ def test_get_static_open_api_definition(responses: RequestsMock, tmpdir):
         match_querystring=True,
     )
 
-    assert generated_functions.open_api_definition_loaded_from_file_get_static_file_call() == [
-        [""]
+    assert (
+        generated_functions.open_api_definition_loaded_from_file_get_static_file_call()
+        == [[""]]
+    )
+
+
+def test_get_static_open_api_definition_as_directory(
+    responses: RequestsMock, tmpdir, caplog
+):
+    caplog.set_level(logging.ERROR, logger="pyxelrest")
+    loader.load(
+        tmpdir,
+        {
+            "open_api_definition_loaded_from_file": {
+                "open_api": {"definition": f"file:///{tmpdir}"},
+                "formulas": {"dynamic_array": {"lock_excel": True}},
+            }
+        },
+    )
+    assert caplog.messages == [
+        f'"open_api_definition_loaded_from_file" will not be available: 400 Client Error: Path Not A File for url: {str(tmpdir).lower()}'
+    ]
+
+
+def test_get_static_open_api_definition_not_found(
+    responses: RequestsMock, tmpdir, caplog
+):
+    caplog.set_level(logging.ERROR, logger="pyxelrest")
+    not_found = os.path.join(tmpdir, "static_open_api_definition.json")
+
+    loader.load(
+        tmpdir,
+        {
+            "open_api_definition_loaded_from_file": {
+                "open_api": {"definition": f"file:///{not_found}"},
+                "formulas": {"dynamic_array": {"lock_excel": True}},
+            }
+        },
+    )
+    assert caplog.messages == [
+        f'"open_api_definition_loaded_from_file" will not be available: 404 Client Error: File Not Found for url: {not_found.lower()}'
+    ]
+
+
+def test_get_static_open_api_definition_relative_path(
+    responses: RequestsMock, tmpdir, caplog
+):
+    caplog.set_level(logging.ERROR, logger="pyxelrest")
+
+    loader.load(
+        tmpdir,
+        {
+            "open_api_definition_loaded_from_file": {
+                "open_api": {"definition": f"file:///ko_existing_file.json"},
+                "formulas": {"dynamic_array": {"lock_excel": True}},
+            }
+        },
+    )
+    import os
+
+    assert caplog.messages == [
+        f'"open_api_definition_loaded_from_file" will not be available: 404 Client Error: File Not Found for url: {os.getcwd()}\\ko_existing_file.json'
     ]
